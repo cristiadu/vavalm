ee914ea080d16f441a3541f6bad062ad81d7446d
Cristiano Faustino <cristiadu@gmail.com>
2023-05-19 20:23:35 -0700

    removing tailwind styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       Welcome to the VaValM website!.
       <Link href="/teams">Team List</Link>
       <Link href="/players">Player List</Link>
