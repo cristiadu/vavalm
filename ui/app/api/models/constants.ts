d48c283b24bfaff5b9f1d3e357022e3f526ec2d0
Cristiano Faustino <cristiadu@gmail.com>
2024-11-18 02:56:56 -0800

    meh

--- ui/app/api/models/constants.ts
+++ ui/app/api/models/constants.ts
@@ -1,8 +1,7 @@
-
 export const LIMIT_PER_PAGE_INITIAL_VALUE: number = 10
 export const PAGE_OFFSET_INITIAL_VALUE: number = 0
 
 export const getApiBaseUrl = () => {
     console.log('process.env.API_BASE_URL', process.env.API_BASE_URL)
-    return  process.env.API_BASE_URL || 'http://localddfsfhost:4000'
+    return process.env.API_BASE_URL || 'http://localhost:4000'
 }
