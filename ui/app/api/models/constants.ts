a2bf49f46ee5e21ff6c95590ee7c8f2460be2078
Cristiano Faustino <cristiadu@gmail.com>
2024-12-07 21:30:09 +0000

    fix ident and dependencies

--- ui/app/api/models/constants.ts
+++ ui/app/api/models/constants.ts
@@ -2,6 +2,5 @@ export const LIMIT_PER_PAGE_INITIAL_VALUE: number = 10
 export const PAGE_OFFSET_INITIAL_VALUE: number = 0
 
 export const getApiBaseUrl = () => {
-    console.log('process.env.API_BASE_URL', process.env.API_BASE_URL)
-    return process.env.API_BASE_URL || 'http://localhost:4000'
+  return process.env.API_BASE_URL || 'http://localhost:8000'
 }
--- ui/next.config.js
+++ ui/next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    // Will be available on both server and client
-    API_BASE_URL: process.env.API_BASE_URL,
-  },
   images: {
     remotePatterns: [
       {
