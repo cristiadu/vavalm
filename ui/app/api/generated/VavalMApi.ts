d65f40d275f356c75670c36b533dcd700aa0cdd5
Cristiano Faustino <cristiadu@gmail.com>
2025-04-05 17:21:40 -0700

    adjusting api path

--- ui/app/api/generated/VavalMApi.ts
+++ ui/app/api/generated/VavalMApi.ts
@@ -12,7 +12,7 @@ export class VavalMApi {
   public readonly request: BaseHttpRequest;
   constructor(config?: Partial<OpenAPIConfig>, HttpRequest: HttpRequestConstructor = FetchHttpRequest) {
     this.request = new HttpRequest({
-      BASE: config?.BASE ?? 'http://localhost:3000/.',
+      BASE: config?.BASE ?? 'http://localhost:8000/.',
       VERSION: config?.VERSION ?? '1.0.0',
       WITH_CREDENTIALS: config?.WITH_CREDENTIALS ?? false,
       CREDENTIALS: config?.CREDENTIALS ?? 'include',
--- ui/app/api/generated/core/OpenAPI.ts
+++ ui/app/api/generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-  BASE: 'http://localhost:3000/.',
+  BASE: 'http://localhost:8000/.',
   VERSION: '1.0.0',
   WITH_CREDENTIALS: false,
   CREDENTIALS: 'include',
